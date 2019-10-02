87158df161cb0dea4cbb6995dd231cf52b6f28b5
Ernesto Rodriguez <ernesto.rodriguez@mercadolibre.com>
2019-10-02 09:26:38 -0300

    remove unnecesary character

--- app/components/uiWeatherWidget/info/index.js
+++ app/components/uiWeatherWidget/info/index.js
@@ -3,12 +3,13 @@ import Temp from './temp';
 import Icon from './icon';
 import Additional from './additional';
 
-class Info extends React.Component {3
-    componentDidMount() { 
-        TweenLite.to('.weather-widget-info__block--icon', .5 , { delay: 2, opacity: 1, y:0}, '+=0.5');  
+class Info extends React.Component {
+    componentDidMount() {
+        TweenLite.to('.weather-widget-info__block--icon', .5, { delay: 2, opacity: 1, y: 0}, '+=0.5');
     }
     render() {
         const { temp, additional, context } = this.props;
+
         return (
             <div className={'weather-widget__info'}>
                <Icon icon={temp.icon}/>
